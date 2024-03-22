3124370dd6f0ab51c4790206040b15eeaa2c2015
Lincon5 <163899868+Lincon5@users.noreply.github.com>
2024-03-21 23:28:08 -0300

    Add files via upload

---
+++ aulao8.py
@@ -0,0 +1,22 @@
+# aula de importação de modulos.
+
+#import math
+#num = int(input('Digite um número:'))
+#raiz = math.sqrt(num)
+#print('A raiz do número  {} é: {:.2f}'.format(num, raiz))
+
+# fazendo com a função MATH.CEIL, que vai arredondar para cima.
+
+#import math
+#num = int(input('Digite um número:'))
+#raiz = math.sqrt(num)
+#print('A raiz do número  {} é: {}'.format(num, math.ceil(raiz)))
+
+#Fazendo com a função MATH.FLOOR, que vai arredondar para baixo.
+
+import math
+num = int(input('Digite um número:'))
+raiz = math.sqrt(num)
+print('A raiz do número  {} é: {}'.format(num, math.floor(raiz)))
+
+# fazendo com a função (from math imoprt) ela serve para utilizar uma função especifica.
